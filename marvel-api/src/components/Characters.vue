6119dcac436a3ab1d576e0cbd7f6ae7c5c70afda
pasteldiventu <jpmmoliveira@gmail.com>
2025-01-27 23:17:19 -0400

    Add Characters component and integrate Axios for API calls

---
+++ marvel-api/src/components/Characters.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div>
+        <h3> componente de personagens </h3>
+    </div>
+</template>
+
+<script>
+import { publicKey, secretKey } from '@/marvel';
+import axios from 'axios';
+export default {
+    name: 'Characters',
+
+    mounted(){
+        this.getCharacters();
+    },
+
+    methods:{
+    getCharacters: function(){
+
+      axios.get(`http://gateway.marvel.com/v1/public/characters?apikey=${publicKey}`)
+        .then((result) => {
+            console.log(result);
+        }) 
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ marvel-api/src/marvel.js
@@ -0,0 +1,2 @@
+export const publicKey = '25b2bff76d4386336328514d8c7d7ce9';
+export const secretKey = 'e52a8ad6c8bc25d5a0b4032a9b4a7517ddd5eac0';
